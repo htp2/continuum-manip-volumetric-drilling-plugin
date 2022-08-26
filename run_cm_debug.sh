ab7fcac52768b867993ab09dc1ce1c1e0e1079bf
Henry <henry.phalen@jhu.edu>
2022-08-26 09:49:39 -0400

    updated launch script for running with imager and removed many extraneous files

--- run_cm_debug.sh
+++ run_cm_debug.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-/home/henry/ambf/bin/lin-x86_64/ambf_simulator --launch_file /home/henry/bigss/catkin_ws/src/continuum-manip-volumetric-drilling-plugin/launch.yaml -l 1,11,7,15,19,20
-
+/home/henry/ambf/bin/lin-x86_64/ambf_simulator \
+    --launch_file /home/henry/bigss/catkin_ws/src/continuum-manip-volumetric-drilling-plugin/launch.yaml \
+    -l 11,34,33,19,20 \
+    --name_body_to_trace Burr \
+    --anatomy_volume_name spine_seg
--- run_ur5_cm.sh
+++ run_ur5_cm.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-/home/henry/ambf/bin/lin-x86_64/ambf_simulator --launch_file /home/henry/bigss/catkin_ws/src/continuum-manip-volumetric-drilling-plugin/launch.yaml -l 15,25,29,30 --name_body_to_trace Burr --anatomy_volume_name spine_seg --csv_filename_static_traces /home/henry/bigss/catkin_ws/src/continuum-manip-volumetric-drilling-plugin/resources/axis.csv
+/home/henry/ambf/bin/lin-x86_64/ambf_simulator \
+    --launch_file /home/henry/bigss/catkin_ws/src/continuum-manip-volumetric-drilling-plugin/launch.yaml \
+    -l 15,25,29,30 \
+    --name_body_to_trace Burr \
+    --anatomy_volume_name spine_seg \
+    --csv_filename_static_traces /home/henry/bigss/catkin_ws/src/continuum-manip-volumetric-drilling-plugin/resources/axis.csv
