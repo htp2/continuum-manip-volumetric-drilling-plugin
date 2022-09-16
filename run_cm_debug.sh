554e1200b69a28b6c96cb3e42c57458ad917f5e6
Joshua Liu <liushuya7@gmail.com>
2022-09-16 19:54:11 -0400

    Fix hardcoded path, to use $CATKIN_WS instead

--- run_cm_debug.sh
+++ run_cm_debug.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-/home/henry/ambf/bin/lin-x86_64/ambf_simulator \
-    --launch_file /home/henry/bigss/catkin_ws/src/continuum-manip-volumetric-drilling-plugin/launch.yaml \
+/home/$USER/ambf/bin/lin-x86_64/ambf_simulator \
+    --launch_file $CATKIN_WS/src/continuum-manip-volumetric-drilling-plugin/launch.yaml \
     -l 11,34,33,19,20 \
     --name_body_to_trace Burr \
     --anatomy_volume_name spine_seg
