0efdc55c98b31302565c69966a2ad18be7d06933
Henry <henry.phalen@jhu.edu>
2022-09-19 17:24:12 -0400

    to run on alienware

--- run_ur5_cm.sh
+++ run_ur5_cm.sh
@@ -8,9 +8,9 @@
 #     --csv_filename_static_traces /home/henry/bigss/catkin_ws/src/continuum-manip-volumetric-drilling-plugin/resources/axis.csv
 
 
-/home/henry/ambf/bin/lin-x86_64/ambf_simulator \
-    --launch_file /home/henry/bigss/catkin_ws/src/continuum-manip-volumetric-drilling-plugin/launch.yaml \
+/home/bigss/ambf/bin/lin-x86_64/ambf_simulator \
+    --launch_file /home/bigss/bigss/catkin_ws/src/continuum-manip-volumetric-drilling-plugin/launch.yaml \
     -l 11,15,25,29,30 \
     --name_body_to_trace seg27 \
     --anatomy_volume_name spine_seg \
-    --csv_filename_static_traces /home/henry/bigss/catkin_ws/src/continuum-manip-volumetric-drilling-plugin/resources/axis.csv
+    --csv_filename_static_traces /home/bigss/bigss/catkin_ws/src/continuum-manip-volumetric-drilling-plugin/resources/axis.csv
